578da0324ec9dd4ea138fa0c5a296bc8c0e1d555
binaryatrocity <brandon.cornejo@gmail.com>
2016-05-21 22:47:35 -0500

    Start from scratch - GitHub upload.

---
+++ Assets/Resources/Scripts/Wand.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using DigitalRuby.PyroParticles;
+
+public class Wand : SteamVR_InteractableObject
+{
+    GameObject bullet;
+    GameObject bulletClone;
+
+    public override void StartUsing(GameObject usingObject)
+    {
+        base.StartUsing(usingObject);
+        FireBullet();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        bullet = this.transform.Find("Firebolt").gameObject;
+    }
+
+    void FireBullet()
+    {
+	bulletClone = Instantiate(bullet, bullet.transform.position, Quaternion.identity) as GameObject;
+	bulletClone.transform.parent = null;
+	bulletClone.SetActive(true);
+    }
+}
