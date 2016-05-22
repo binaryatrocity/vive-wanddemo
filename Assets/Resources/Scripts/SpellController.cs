6f7a16df45a918a0f952c751e7884e09c715624a
binaryatrocity <brandon.cornejo@gmail.com>
2016-05-22 14:03:47 -0500

    Add spellController. Add spell audio file

---
+++ Assets/Resources/Scripts/SpellController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellController : MonoBehaviour {
+
+	public bool isProjectile  = false;
+	public float projectileSpeed = 20f;
+	public Vector3 startPosition = Vector3.forward;
+	public Vector3 startDirection = Vector3.forward;
+
+	public AudioSource castingAudio;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = startPosition;
+		castingAudio.Play();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (isProjectile) {
+			transform.Translate(startDirection * (projectileSpeed * Time.deltaTime));
+		}
+	}
+}
